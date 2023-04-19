164fd0bc8d8eaf461e7e39baee3a21355f98542d
Vitalii_Stetsenko <mrcmert19980701@gmail.com>
2023-04-19 18:21:59 +0300

    change ip

--- HW6_pytest/pytest/connection.py
+++ HW6_pytest/pytest/connection.py
@@ -4,7 +4,7 @@ import pymssql
 
 @pytest.fixture(scope="module")
 def db_connection():
-    conn = pymssql.connect(server=r'127.0.0.1',
+    conn = pymssql.connect(server=r'172.19.32.1',
                            port='1433',
                            user=r'NewLogin',
                            password=r'1234567890',
